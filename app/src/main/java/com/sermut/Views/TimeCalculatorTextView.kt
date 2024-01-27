7df46f5dab1a8cc0de4ee6239d5cf0cb0489a04e
SergejH1990 <seho1990@gmx.de>
2024-01-27 20:59:09 +0100

    Add custom text view

---
+++ app/src/main/java/com/sermut/Views/TimeCalculatorTextView.kt
@@ -0,0 +1,8 @@
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class TimeCalculatorTextView : View
+{
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+}
--- app/src/main/java/com/sermut/timecalculator/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/sermut/timecalculator/ui/calculator/CalculatorFragment.kt
@@ -10,7 +10,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.sermut.timecalculator.databinding.FragmentCalculatorBinding
 import com.sermut.timecalculator.databinding.FragmentCalendarBinding
 
-class CalculatorFragment : Fragment() {
+class CalculatorFragment : Fragment() 
+{
 
     private var _binding: FragmentCalculatorBinding? = null
 
@@ -21,24 +22,25 @@ class CalculatorFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val calculatorViewModel =
-            ViewModelProvider(this).get(CalculatorViewModel::class.java)
+        savedInstanceState: Bundle?): View 
+    {
+        val calculatorViewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
 
         _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textCalculator
         val textView1: TextView = binding.textCalculator1
-        calculatorViewModel.text.observe(viewLifecycleOwner) {
+        calculatorViewModel.text.observe(viewLifecycleOwner) 
+        {
             textView.text = it
             textView1.text = it
         }
         return root
     }
 
-    override fun onDestroyView() {
+    override fun onDestroyView() 
+    {
         super.onDestroyView()
         _binding = null
     }
