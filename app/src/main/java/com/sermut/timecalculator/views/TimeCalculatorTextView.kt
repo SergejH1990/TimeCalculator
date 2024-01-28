a051ebe199bbec0e9c7fd5334397089059498911
SergejH1990 <seho1990@gmx.de>
2024-01-28 02:58:16 +0100

    Inflate layout for custom widget.

--- app/src/main/java/com/sermut/timecalculator/views/TimeCalculatorTextView.kt
+++ app/src/main/java/com/sermut/timecalculator/views/TimeCalculatorTextView.kt
@@ -2,9 +2,11 @@ package com.sermut.timecalculator.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import android.widget.CheckBox
+import com.sermut.timecalculator.R
 
 class TimeCalculatorTextView : View
 {
@@ -14,15 +16,20 @@ class TimeCalculatorTextView : View
 
     constructor(context: Context) : super(context)
     {
-        yearsTextView = TextView(context)
-        daysTextView = TextView(context)
-        yearsCheckBox = CheckBox(context)
+        init()
     }
 
     constructor(context: Context, attribs: AttributeSet) : super(context, attribs)
     {
-        yearsTextView = TextView(context, attribs)
-        daysTextView = TextView(context, attribs)
-        yearsCheckBox = CheckBox(context, attribs)
+        init()
+    }
+
+    fun init()
+    {
+        LayoutInflater.from(context).inflate(R.layout.time_calculator_text_view, null, true)
+
+        yearsTextView = findViewById(R.id.years_text_view)
+        daysTextView = findViewById(R.id.days_text_view)
+        yearsCheckBox = findViewById(R.id.years_checkbox)
     }
 }
