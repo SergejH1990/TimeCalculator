d2bfaa0639533af4ee0d82c9a208c6e8e469a63b
SergejH1990 <seho1990@gmx.de>
2024-01-28 10:53:45 +0100

    Fixed visibility of composite view

--- app/src/main/java/com/sermut/timecalculator/views/TimeCalculatorTextView.kt
+++ app/src/main/java/com/sermut/timecalculator/views/TimeCalculatorTextView.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import android.widget.CheckBox
+import android.widget.LinearLayout
 import com.sermut.timecalculator.R
 
-class TimeCalculatorTextView : View
+class TimeCalculatorTextView : LinearLayout
 {
     var yearsTextView : TextView? = null
     var daysTextView : TextView? = null
@@ -24,12 +25,23 @@ class TimeCalculatorTextView : View
         init()
     }
 
-    fun init()
+    private fun init()
     {
-        LayoutInflater.from(context).inflate(R.layout.time_calculator_text_view, null, true)
+        inflate(context, R.layout.time_calculator_text_view, this)
 
         yearsTextView = findViewById(R.id.years_text_view)
+        yearsTextView?.visibility = View.GONE
         daysTextView = findViewById(R.id.days_text_view)
         yearsCheckBox = findViewById(R.id.years_checkbox)
+
+        yearsCheckBox?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+            {
+                yearsTextView?.visibility = View.VISIBLE
+                return@setOnCheckedChangeListener
+            }
+
+            yearsTextView?.visibility = View.GONE
+        }
     }
 }
